ae8c296b3979b1428311ad4db3f304db89db2838
Nick Barth <nickbarth@live.com>
2014-08-04 07:41:13 -0600

    Update iOSContacts.m

--- src/ios/iOSContacts.m
+++ src/ios/iOSContacts.m
@@ -7,42 +7,41 @@
 - (void)iOSContacts:(CDVInvokedUrlCommand *)command {
     self.callbackId = command.callbackId;
 
-
     CFErrorRef * error = NULL;
     ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, error);
     ABAddressBookRequestAccessWithCompletion(addressBook, ^(bool granted, CFErrorRef error) {
-       
       if (granted) {
           dispatch_async(dispatch_get_main_queue(), ^{
-              CFArrayRef allContacts = ABAddressBookCopyArrayOfAllPeople(addressBook);
-              CFIndex numOfContacts = ABAddressBookGetPersonCount(addressBook);
-              // NSString *contacts = @"";
+              NSArray *allContacts = (__bridge_transfer NSArray *)ABAddressBookCopyArrayOfAllPeople(addressBook);
 
-              if (numOfContacts > 0) {
+              if ([allContacts count] > 0) {
                   NSString *result = [NSString stringWithFormat: @"{ \"error\": false, \"contacts\": ["];
-
-                  for(int i = 0; i < numOfContacts; i++){
-                      ABRecordRef person = CFArrayGetValueAtIndex(allContacts, i);
+                  for(int i = 0; i < [allContacts count]; i++){
+                      ABRecordRef person = (__bridge ABRecordRef)[allContacts objectAtIndex:i];
 
                       NSString *firstName = (__bridge NSString *)(ABRecordCopyValue(person, kABPersonFirstNameProperty));
                       NSString *lastName = (__bridge NSString *)(ABRecordCopyValue(person, kABPersonLastNameProperty));
                       NSString *phoneNumber = @"";
-
+                      NSString *phoneLabel = @"";
+                      
                       ABMultiValueRef phoneNumbers = ABRecordCopyValue(person, kABPersonPhoneProperty);
 
-                      NSMutableArray *numbers = [NSMutableArray array];
-                      for (CFIndex i = 0; i < ABMultiValueGetCount(phoneNumbers); i++) {
-                          phoneNumber = (__bridge_transfer NSString *) ABMultiValueCopyValueAtIndex(phoneNumbers, i);
-                          // [numbers addObject:phoneNumber];
+                      for (CFIndex n = 0; n < ABMultiValueGetCount(phoneNumbers); n++) {
+                          phoneLabel = (__bridge_transfer NSString *) ABMultiValueCopyLabelAtIndex(phoneNumbers, n);
+                          
+                          if ([phoneLabel isEqualToString:(NSString *)kABPersonPhoneMobileLabel]) {
+                              phoneNumber = (__bridge_transfer NSString *) ABMultiValueCopyValueAtIndex(phoneNumbers, n);
+                              break;
+                          }
                       }
-
-                      // NSMutableDictionary *contact = [NSMutableDictionary dictionary];
-                      // [contact setObject:name forKey:@"name"];
-                      // [contact setObject:numbers forKey:@"numbers"];
-                      result = [NSString stringWithFormat:@"%@ { \"name\": \"%@ %@\", \"number\": \"%@\" },", result, firstName, lastName, phoneNumber];
+                      
+                      if (![phoneNumber isEqualToString:@""]) {
+                          result = [NSString stringWithFormat:@"%@ { \"name\": \"%@ %@\", \"number\": \"%@\" },", result, firstName, lastName, phoneNumber];
+                      }
                   }
 
                   result = [NSString stringWithFormat:@"%@] }", [result substringToIndex:[result length] - 1]];
+                  
                   CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:result];
                   NSString* javaScript = [pluginResult toSuccessCallbackString:self.callbackId];
                   [self writeJavascript:javaScript];
